92d83bad915e2155171d491c3d86e9e584d52ffd
WiViW <wiviw.public@gmail.com>
2023-09-05 23:38:11 -0500

    Sound update!
    Now plays sounds whenever you receive an update.

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ const wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 register("Chat", (event) => {
 	var formattedMessage = ChatLib.getChatMessage(event, true);
 	if (formattedMessage.includes("&6&lACTIVE PLAYER! &eYou gained &b+1 Raffle Ticket&e!&r")) {
-		wait(1170000).then(() => ChatLib.chat(new TextComponent('&b[RAFFLE] &e30 Second Warning for: &aActive&e!')))
-		wait(1200000).then(() => ChatLib.chat(new TextComponent('&b[RAFFLE] &e20 Minute timer up for: &aActive&e!')))
+		InitiateCountdown();
 	}
 })
+
+function InitiateCountdown() {
+	wait(1170000).then(() => {
+		World.playSound("note.harp", 100, 2);
+		ChatLib.chat(new TextComponent('&b[RAFFLE] &e30 Second Warning forfor: &aActive&e!'))
+	})
+	wait(1200000).then(() => {
+		World.playSound("note.pling", 100, 2);
+		ChatLib.chat(new TextComponent('&b[RAFFLE] &e20 Minute timer up for: &aActive&e!'))
+	})
+}
